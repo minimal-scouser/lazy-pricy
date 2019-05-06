6d7c5328e3b0efc95bb0bcee424fcc1a4db9dff9
Syed Sharukh Pasha <sharukhpasha.lfc@gmail.com>
2019-05-06 17:12:46 +0530

    Update settings.py

--- lazyPricy/settings.py
+++ lazyPricy/settings.py
@@ -16,6 +16,8 @@ SETTINGS_DIR = os.path.dirname(__file__)
 PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
 PROJECT_PATH = os.path.abspath(PROJECT_PATH)
 
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'staticfiles')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
