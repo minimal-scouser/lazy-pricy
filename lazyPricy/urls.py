e3b1ab7cc051422733a8f27a06f9303de0145112
Syed Sharukh Pasha <sharukhpasha.lfc@gmail.com>
2019-05-06 17:24:47 +0530

    Added '' to path

--- lazyPricy/urls.py
+++ lazyPricy/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from costentha.views import getCity, findArea, findPrice
 
 urlpatterns = [
+    path('', getCity)
     path('admin/', admin.site.urls),
     path('lazyPricy/getCity/', getCity),
     path('lazyPricy/getCity/findArea/', findArea),
