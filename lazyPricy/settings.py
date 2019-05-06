3e2c924d14357a141afef412055b4b74d44612d9
Syed Sharukh Pasha <sharukhpasha.lfc@gmail.com>
2019-05-06 17:05:53 +0530

    Update settings.py

--- lazyPricy/settings.py
+++ lazyPricy/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -130,4 +131,4 @@ STATICFILES_DIRS = (
     STATIC_PATH,
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
